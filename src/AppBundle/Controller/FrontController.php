9da594fe0cba44585ca0470464f60eef8d0beea5
repelmoine <pelmoine.remi@gmail.com>
2017-05-20 14:49:13 +0200

    home page

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -15,7 +15,7 @@ class FrontController extends Controller
     {
         $render = 'default/index.html.twig';
         // replace this example code with whatever you need
-        if(!in_array($this->get('client.informations')->get_client_ip(),["127.0.0.1","90.4.244.231","88.188.200.205","37.171.14.131","78.121.74.3"])){
+        if(!in_array($this->get('client.informations')->get_client_ip(),["127.0.0.1","86.216.96.219","88.188.200.205","37.171.14.131","78.121.74.3"])){
             $render = 'errors/maintenance.html.twig';
         }
         return $this->render($render, [
